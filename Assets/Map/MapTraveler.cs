35182fb1a3ed2a4795c0b88f82b868fd4e639edc
Ben Wander <ben@thewandering.band>
2021-05-16 11:39:50 -0600

    Call CustomTexture update manually to make more efficient

--- Assets/Map/MapTraveler.cs
+++ Assets/Map/MapTraveler.cs
@@ -2,26 +2,26 @@ using UnityEngine;
 
 public class MapTraveler : MonoBehaviour
 {
-    [SerializeField] RenderTexture mapLinesRenderTexture;
+    [SerializeField] CustomRenderTexture mapLinesRenderTexture;
     [SerializeField] Material mapLinesMaterial;
     [SerializeField] Vector2 worldSize;
 
+    Vector3 position = Vector3.zero;
+
     void Start()
     {
-        ClearRenderTexture();
+        mapLinesRenderTexture.Initialize();
     }
 
     void Update()
     {
-        mapLinesMaterial.SetVector("_Coordinate", new Vector4(transform.position.x/worldSize.x, 
-                                                                transform.position.z/worldSize.y, 0, 0));
-    }
-    
-    void ClearRenderTexture()
-    {
-        RenderTexture rt = RenderTexture.active;
-        RenderTexture.active = mapLinesRenderTexture;
-        GL.Clear(true, true, Color.clear);
-        RenderTexture.active = rt;
+        if (position.x != transform.position.x || position.z != transform.position.z)
+        {
+            mapLinesMaterial.SetVector("_Coordinate", new Vector4(transform.position.x / worldSize.x,
+                transform.position.z / worldSize.y, 0, 0));
+            
+            position = transform.position;
+            mapLinesRenderTexture.Update();
+        }
     }
 }
